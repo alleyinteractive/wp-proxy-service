d0e218bf49fe1d4c02cbec4c6802da03c046b06d
Emily Leffler Schulman <24902269+emilyatmobtown@users.noreply.github.com>
2023-12-07 16:03:28 -0500

    Basic setup.

---
+++ src/class-wp-proxy-service.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * WP_Proxy_Service class file
+ *
+ * @package wp-proxy-service
+ */
+
+declare(strict_types = 1);
+
+namespace Alley\WP\Proxy_Service;
+
+/**
+ * WP_Proxy_Service class.
+ */
+class WP_Proxy_Service {
+
+}
