c4297649ba784c5ef8b8e95590e793c92497b55b
Emily Leffler Schulman <24902269+emilyatmobtown@users.noreply.github.com>
2023-12-07 20:51:55 -0500

    Filter REST request.

--- src/class-wp-proxy-service.php
+++ src/class-wp-proxy-service.php
@@ -14,4 +14,11 @@ namespace Alley\WP\Proxy_Service;
  */
 class WP_Proxy_Service {
 
+	public function init(): void {
+		add_filter( 'rest_request_before_callbacks', [ $this, 'filter_rest_request_before_callbacks' ], 10, 3 );
+	}
+
+	public function filter_rest_request_before_callbacks( $return, $request, $route ) {
+		return $response;
+	}
 }
